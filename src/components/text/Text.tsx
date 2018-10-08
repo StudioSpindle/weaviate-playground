35179fec5a5a1575c08649226568df5ce4948246
Dominique Collette <mail@dominiquecollette.com>
2018-10-08 22:13:01 +0200

    gh-14: add Text component

---
+++ src/components/text/Text.tsx
@@ -0,0 +1,88 @@
+import * as React from 'react';
+import getColor, { Color } from 'src/utils/getColor';
+import getTextSize from 'src/utils/getTextSize';
+import styled from 'styled-components';
+
+export type FontStyle = 'normal' | 'italic';
+export type FontWeight = 'regular' | 'bold' | 'light';
+export type TextTransform = 'capitalize' | 'lowercase' | 'uppercase' | 'none';
+export type TextWrap = 'normal' | 'noWrap' | 'breakWord' | 'ellipsis';
+export type TextComponent =
+  | 'h1'
+  | 'h2'
+  | 'h3'
+  | 'h4'
+  | 'h5'
+  | 'h6'
+  | 'p'
+  | 'span';
+
+export interface IText {
+  className?: string;
+  color?: Color;
+  component?: TextComponent;
+  fontStyle?: FontStyle;
+  fontWeight?: FontWeight;
+  gutter?: boolean;
+  textTransform?: TextTransform;
+  textWrap?: TextWrap;
+}
+
+export const getTextWrapCss = (textWrap: IText['textWrap']): string => {
+  switch (textWrap) {
+    case 'noWrap':
+      return 'white-space: nowrap;';
+    case 'breakWord':
+      return 'word-wrap: break-word;';
+    case 'ellipsis':
+      return `
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+      `;
+    case 'normal':
+    default:
+      return '';
+  }
+};
+
+export const getTextCss = (props: IText): string => `
+  font-family: "Alegreya Sans",sans-serif;
+  font-kerning: auto;
+  font-variant-ligatures: common-ligatures;
+  line-height: 1.55;
+  ${getTextWrapCss(props.textWrap)}
+  ${getTextSize(props.component)}
+  color: ${getColor(props.color as Color)};
+  ${props.fontStyle ? `font-style: ${props.fontStyle};` : ''}
+  margin: ${props.gutter ? `0 0 1em` : '0'};
+  padding: 0;
+  ${props.textTransform ? `text-transform: ${props.textTransform};` : ''}
+`;
+
+const Text: React.SFC<IText> = ({
+  component,
+  children,
+  color,
+  gutter,
+  textTransform,
+  fontStyle,
+  fontWeight,
+  textWrap,
+  ...rest
+}) => {
+  const Component = component!;
+  return <Component {...rest}>{children}</Component>;
+};
+
+Text.defaultProps = {
+  color: 'almostBlack',
+  component: 'span',
+  fontWeight: 'regular',
+  gutter: false,
+  textWrap: 'normal'
+} as IText;
+
+export default styled(Text)`
+  ${getTextCss};
+`;

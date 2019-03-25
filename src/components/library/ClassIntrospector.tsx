c8e8c6e6dda16e352f561d3be09802493e9c95bb
Dominique Collette <mail@dominiquecollette.com>
2019-03-25 19:20:35 +0100

    gh-104: remove empty Weaviate message

--- src/components/library/ClassIntrospector.tsx
+++ src/components/library/ClassIntrospector.tsx
@@ -30,6 +30,10 @@ interface IClassIntrospectorState {
   loading: boolean;
 }
 
+interface IClassFetcher {
+  isWeaviateEmpty: boolean;
+}
+
 const StateMessage = ({
   message,
   state
@@ -93,144 +97,153 @@ const StateMessage = ({
   </Grid>
 );
 
-const ClassFetcher: React.SFC = ({ children }) => (
-  <ClassIdsQuery query={CLASS_IDS_QUERY}>
-    {classIdsQuery => {
-      return (
-        <LocalClassesQuery
-          query={LOCAL_CLASSES_QUERY}
-          variables={{ typename: 'WeaviateLocalGetObj' }}
-        >
-          {localClassesQuery => {
-            if (localClassesQuery.loading) {
-              return (
-                <StateMessage
-                  state="loading"
-                  message={translations.loadingLocalClasses}
-                />
-              );
-            }
+const ClassFetcher: React.SFC<IClassFetcher> = ({
+  children,
+  isWeaviateEmpty
+}) => {
+  if (isWeaviateEmpty) {
+    return <React.Fragment>{children}</React.Fragment>;
+  }
 
-            if (localClassesQuery.error) {
-              return (
-                <StateMessage
-                  state="error"
-                  message={localClassesQuery.error.message}
-                />
-              );
-            }
+  return (
+    <ClassIdsQuery query={CLASS_IDS_QUERY}>
+      {classIdsQuery => {
+        return (
+          <LocalClassesQuery
+            query={LOCAL_CLASSES_QUERY}
+            variables={{ typename: 'WeaviateLocalGetObj' }}
+          >
+            {localClassesQuery => {
+              if (localClassesQuery.loading) {
+                return (
+                  <StateMessage
+                    state="loading"
+                    message={translations.loadingLocalClasses}
+                  />
+                );
+              }
 
-            if (localClassesQuery.data && localClassesQuery.data.__type) {
-              localClassesQuery.data.__type.fields.forEach(
-                localGetCLASSTYPEObj => {
-                  const classType = localGetCLASSTYPEObj.name;
-                  localGetCLASSTYPEObj.type.fields.forEach(CLASS => {
-                    /**
-                     * Store class information on client
-                     */
-                    apolloClient.mutate({
-                      mutation: UPDATE_CLASS_MUTATION,
-                      variables: {
-                        classLocation: 'Local',
-                        classType,
-                        filters: '{}',
-                        id: `local-${classType}-${CLASS.name}`,
-                        instance: 'Local',
-                        name: CLASS.name
-                      }
+              if (localClassesQuery.error) {
+                return (
+                  <StateMessage
+                    state="error"
+                    message={localClassesQuery.error.message}
+                  />
+                );
+              }
+
+              if (localClassesQuery.data && localClassesQuery.data.__type) {
+                localClassesQuery.data.__type.fields.forEach(
+                  localGetCLASSTYPEObj => {
+                    const classType = localGetCLASSTYPEObj.name;
+                    localGetCLASSTYPEObj.type.fields.forEach(CLASS => {
+                      /**
+                       * Store class information on client
+                       */
+                      apolloClient.mutate({
+                        mutation: UPDATE_CLASS_MUTATION,
+                        variables: {
+                          classLocation: 'Local',
+                          classType,
+                          filters: '{}',
+                          id: `local-${classType}-${CLASS.name}`,
+                          instance: 'Local',
+                          name: CLASS.name
+                        }
+                      });
                     });
-                  });
-                }
+                  }
+                );
+              }
+
+              return (
+                <NetworkClassesQuery
+                  query={NETWORK_CLASSES_QUERY}
+                  variables={{ typename: 'WeaviateNetworkGetObj' }}
+                >
+                  {networkClassesQuery => {
+                    if (networkClassesQuery.loading) {
+                      return (
+                        <StateMessage
+                          state="loading"
+                          message={translations.loadingNetworkClasses}
+                        />
+                      );
+                    }
+                    if (networkClassesQuery.error) {
+                      return (
+                        <StateMessage
+                          state="error"
+                          message={translations.defaultError}
+                        />
+                      );
+                    }
+
+                    if (
+                      networkClassesQuery.data &&
+                      networkClassesQuery.data.__type
+                    ) {
+                      networkClassesQuery.data.__type.fields.forEach(
+                        networkGetINSTANCEObj => {
+                          const instance = networkGetINSTANCEObj.name;
+                          networkGetINSTANCEObj.type.fields.forEach(
+                            networkGetINSTANCECLASSTYPEObj => {
+                              const classType =
+                                networkGetINSTANCECLASSTYPEObj.name;
+                              networkGetINSTANCECLASSTYPEObj.type.fields.forEach(
+                                CLASS => {
+                                  /**
+                                   * Store class information on client
+                                   */
+                                  apolloClient.mutate({
+                                    mutation: UPDATE_CLASS_MUTATION,
+                                    variables: {
+                                      classLocation:
+                                        instance === 'Local'
+                                          ? instance
+                                          : 'Network',
+                                      classType,
+                                      filters: '{}',
+                                      id: `${instance}-${classType}-${
+                                        CLASS.name
+                                      }`,
+                                      instance,
+                                      name: CLASS.name
+                                    }
+                                  });
+                                }
+                              );
+                            }
+                          );
+                        }
+                      );
+                    }
+
+                    if (
+                      (!localClassesQuery.data ||
+                        !localClassesQuery.data.__type) &&
+                      (!networkClassesQuery.data ||
+                        !networkClassesQuery.data.__type)
+                    ) {
+                      return (
+                        <StateMessage
+                          state="error"
+                          message={translations.defaultError}
+                        />
+                      );
+                    }
+
+                    return children;
+                  }}
+                </NetworkClassesQuery>
               );
-            }
-
-            return (
-              <NetworkClassesQuery
-                query={NETWORK_CLASSES_QUERY}
-                variables={{ typename: 'WeaviateNetworkGetObj' }}
-              >
-                {networkClassesQuery => {
-                  if (networkClassesQuery.loading) {
-                    return (
-                      <StateMessage
-                        state="loading"
-                        message={translations.loadingNetworkClasses}
-                      />
-                    );
-                  }
-                  if (networkClassesQuery.error) {
-                    return (
-                      <StateMessage
-                        state="error"
-                        message={translations.defaultError}
-                      />
-                    );
-                  }
-
-                  if (
-                    networkClassesQuery.data &&
-                    networkClassesQuery.data.__type
-                  ) {
-                    networkClassesQuery.data.__type.fields.forEach(
-                      networkGetINSTANCEObj => {
-                        const instance = networkGetINSTANCEObj.name;
-                        networkGetINSTANCEObj.type.fields.forEach(
-                          networkGetINSTANCECLASSTYPEObj => {
-                            const classType =
-                              networkGetINSTANCECLASSTYPEObj.name;
-                            networkGetINSTANCECLASSTYPEObj.type.fields.forEach(
-                              CLASS => {
-                                /**
-                                 * Store class information on client
-                                 */
-                                apolloClient.mutate({
-                                  mutation: UPDATE_CLASS_MUTATION,
-                                  variables: {
-                                    classLocation:
-                                      instance === 'Local'
-                                        ? instance
-                                        : 'Network',
-                                    classType,
-                                    filters: '{}',
-                                    id: `${instance}-${classType}-${
-                                      CLASS.name
-                                    }`,
-                                    instance,
-                                    name: CLASS.name
-                                  }
-                                });
-                              }
-                            );
-                          }
-                        );
-                      }
-                    );
-                  }
-
-                  if (
-                    (!localClassesQuery.data ||
-                      !localClassesQuery.data.__type) &&
-                    (!networkClassesQuery.data ||
-                      !networkClassesQuery.data.__type)
-                  ) {
-                    return (
-                      <StateMessage
-                        state="error"
-                        message={translations.defaultError}
-                      />
-                    );
-                  }
-
-                  return children;
-                }}
-              </NetworkClassesQuery>
-            );
-          }}
-        </LocalClassesQuery>
-      );
-    }}
-  </ClassIdsQuery>
-);
+            }}
+          </LocalClassesQuery>
+        );
+      }}
+    </ClassIdsQuery>
+  );
+};
 
 /**
  * ClassIntrospector: introspects and stores classes to client
@@ -292,16 +305,9 @@ class ClassIntrospector extends React.Component<
           message="The provided url doesn't provide access to a Weaviate instance."
         />
       );
-    } else if (empty) {
-      return (
-        <StateMessage
-          state="error"
-          message="The provided Weaviate instance is empty."
-        />
-      );
     }
 
-    return <ClassFetcher children={children} />;
+    return <ClassFetcher isWeaviateEmpty={empty} children={children} />;
   }
 }
 

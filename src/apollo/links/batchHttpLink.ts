5af2611687a6b4560c71820d97da924cd334e4eb
Dominique Collette <mail@dominiquecollette.com>
2019-04-17 17:24:10 +0200

    gh-145: add debounce to D3 tick & reuse forceManyBody call

--- src/apollo/links/batchHttpLink.ts
+++ src/apollo/links/batchHttpLink.ts
@@ -2,9 +2,6 @@ import { BatchHttpLink } from 'apollo-link-batch-http';
 
 const urlParams = new URLSearchParams(window.location.search);
 const uri = urlParams.get('weaviateUri') || '';
-
-// tslint:disable-next-line:no-console
-console.log(uri);
 const batchUri = `${uri}/batch`;
 
 const batchHttpLink = new BatchHttpLink({

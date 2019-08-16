71e43b4e43196005a049320689ea93963579e821
Etienne Dilocker <etienne@dilocker.de>
2019-08-16 13:18:09 +0200

    update url structure in helper message

--- src/translations/en.ts
+++ src/translations/en.ts
@@ -3,7 +3,7 @@ const translations = {
   errorAnonymousAccess:
     'Anonymous access not enabled, please provide an auth scheme such as OIDC.',
   errorWrongUrl:
-    'No Weaviate found at this URL. Please make sure to use: http://your-weaviate-url:port/weaviate/v1/graphql.',
+    'No Weaviate found at this URL. Please make sure to use: http://your-weaviate-url:port/v1/graphql.',
   filterRangeSliderNoMinMax:
     'Your have defined a number type without min and/or max value.',
   loadingLocalClasses: 'Loading local classes...',
